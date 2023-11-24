4280563c39817dec42ffbd1d23bb5cd04f2ee221
Victor de Souza Solique <victordesouzasolique@gmail.com>
2023-11-24 16:03:46 -0400

    dia 24/11
    Primeiro commit, fazendo apenas a estilização, menos o registro de videos, só coloquei o Home e os Destaques na sua forma final.

---
+++ src/Destaque.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
+
+export default function Destaque() {
+    return(
+    <div className="container-xl bg-cinza pt-4">
+        
+        <h1 className="text-start">Músicas Destaques <span class="px-2 fs-3 bg-danger-subtle">Tema: Sertanejo</span></h1>   
+        <nav aria-label="breadcrumb">
+            <ol className="breadcrumb">
+                <li className="breadcrumb-item"><Link to="/">Home</Link></li>
+                <li className="breadcrumb-item active" aria-current="page">Em Destaque</li>
+                <li className="breadcrumb-item active" aria-current="page"><Link to="/registro">Registrar Vídeo</Link></li>
+            </ol>
+        </nav>
+        <h4 className="fw-bold pb-0 mb-0">Todos os Vídeos</h4>
+        <div class="container text-center">
+            <div class="row row-cols-2">
+                <div class="col">
+                    <div class="bg-danger ratio ratio-4x3">
+                        <div>1x1</div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="ratio ratio-4x3">
+                        <div>1x1</div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="ratio ratio-4x3">
+                        <div>1x1</div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="ratio ratio-4x3">
+                        <div>1x1</div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+            
+        
+    </div>
+    );
+}
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,5 +1,51 @@
-export default function Home(){
-    return(
-        <h1>PTAC 2 - PROJETO FINAL</h1>
-     );
+import { useState } from "react";
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
+
+export default function Home() {
+    
+
+    return (
+    <div className="container-xl bg-cinza pt-4">
+        
+        <h1 className="text-start">Galeria de Músicas <span class="px-2 fs-3 bg-danger-subtle">Tema: Sertanejo</span></h1>   
+         
+        <nav aria-label="breadcrumb">
+            <ol className="breadcrumb">
+                <li className="breadcrumb-item">Home</li>
+                <li className="breadcrumb-item" aria-current="page"><Link to="/destaque">Em Destaque</Link></li>
+                <li className="breadcrumb-item" aria-current="page"><Link to="/registro">Registrar Vídeo</Link></li>
+            </ol>
+        </nav>
+        <h4 className="fw-bold pb-0 mb-0">Todos os Vídeos</h4>
+        <div class="container text-center">
+            <div class="row row-cols-3">
+                <div class="col">
+                    <div class="bg-danger ratio ratio-4x3">
+                        <div>1x1</div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="ratio ratio-4x3">
+                        <div>1x1</div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="ratio ratio-4x3">
+                        <div>1x1</div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="ratio ratio-4x3">
+                        <div>1x1</div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+            
+        
+    </div>
+    );
 }
