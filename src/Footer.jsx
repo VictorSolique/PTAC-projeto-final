fb2e5478f9f092e69d1e967b60d0c54bbcced292
Victor de Souza Solique <victordesouzasolique@gmail.com>
2023-12-07 15:22:52 -0400

    dia 07/12
    Mexendo mais um pouco no footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,7 +3,7 @@ export default function Footer() {
         <div className="pt-5 mt-5">
             <div className="mt-5 text-center bg-dark-subtle">
                 <div className="text-center p-3">
-                    <p>© 2020 Copyright: MDBootstrap.com</p>
+                    <p>© 2016-2023 Galeria de Músicas L.T.D.A Todos os direitos reservados.</p>
                 </div>
             </div>
         </div>
